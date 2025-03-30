62a25446c22505d520d60e94563f6090c32b28f9
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2025-03-31 03:02:54 +0530

    Release version-0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
-use std::{process, error::Error};
+//! # lorenz-cli
+//!
+//! Plots lorenz attractor in terminal.
+//! 
+//! # Exit
+//! to terminate the plotter enter `q` or `Esc`
+
+use std::{error::Error, process};
 
 const SIGMA: f64 = 10.0;
 const RHO: f64 = 28.0;
@@ -8,9 +15,9 @@ pub fn generate_lorenz_points() -> Vec<(f64, f64)> {
     let dt = 0.01;
     let num_steps = 10000;
 
-    let mut x = 0.0;
-    let mut y = 1.0;
-    let mut z = 1.05;
+    let mut x = 5.0;
+    let mut y = 18.0;
+    let mut z = 10.0;
     let mut points = Vec::with_capacity(num_steps);
 
     for _ in 0..num_steps {
@@ -21,17 +28,22 @@ pub fn generate_lorenz_points() -> Vec<(f64, f64)> {
         x += dx * dt;
         y += dy * dt;
         z += dz * dt;
-        points.push((x, z));
+        points.push((x, y));
     }
     points
 }
 
-pub fn lorenz (x:f64, y:f64, z:f64, sigma:f64, rho:f64, beta:f64) 
--> Result<(f64, f64, f64),Box<dyn Error>>
-{
-    let dx = sigma*(y-x);
-    let dy = x*(rho-z) - y;
-    let dz = x*y - beta*z;
+pub fn lorenz(
+    x: f64,
+    y: f64,
+    z: f64,
+    sigma: f64,
+    rho: f64,
+    beta: f64,
+) -> Result<(f64, f64, f64), Box<dyn Error>> {
+    let dx = sigma * (y - x);
+    let dy = x * (rho - z) - y;
+    let dz = x * y - beta * z;
 
-    Ok((dx, dy, dz)) 
+    Ok((dx, dy, dz))
 }
--- src/main.rs
+++ src/main.rs
@@ -9,93 +9,117 @@ use ratatui::{
     layout::Rect,
     style::{Color, Style},
     symbols,
-    widgets::{Axis, Block, Borders, Chart, Dataset},
+    widgets::{Axis, Chart, Dataset},
 };
 use std::{
     io::{Result, stdout},
-    process, thread,
+    process,
     time::Duration,
 };
 
-use lorenz::lorenz;
+use lorenz_cli::lorenz;
 
 const SIGMA: f64 = 10.0;
 const RHO: f64 = 28.0;
 const BETA: f64 = 8.0 / 3.0;
 
 fn main() -> Result<()> {
-
     enable_raw_mode()?;
     let mut stdout = stdout();
     execute!(stdout, EnterAlternateScreen)?;
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
+    let axis_choice = std::env::args().nth(1).unwrap_or_else(|| "xy".to_string());
+
     let dt = 0.01;
     let num_steps = 10000;
 
-    let mut x = 0.0;
-    let mut y = 1.0;
-    let mut z = 1.05;
-    let mut points: Vec<(f64, f64)> = Vec::new();
+    'lorenz_attractor: loop {
+        let mut x = 6.0;
+        let mut y = 1.0;
+        let mut z = 1.05;
+        let mut points: Vec<(f64, f64)> = Vec::new();
 
-    let mut step = 0;
+        let mut step = 0;
+        // let points = generate_lorenz_points();
 
-    loop {
-        if step < num_steps {
+        let mut x_min = f64::MAX;
+        let mut x_max = f64::MIN;
+        let mut y_min = f64::MAX;
+        let mut y_max = f64::MIN;
+
+        while step < num_steps {
             let (dx, dy, dz) = lorenz(x, y, z, SIGMA, RHO, BETA).unwrap_or_else(|err| {
+                disable_raw_mode().ok();
+                execute!(terminal.backend_mut(), LeaveAlternateScreen).ok();
                 eprintln!("{:?}", err);
                 process::exit(1);
             });
+            if dx.abs() > 1000.0 || dy.abs() > 1000.0 || dz.abs() > 1000.0 {
+                disable_raw_mode()?;
+                execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+                eprintln!("Diverging Lorenz Attractor! Exiting...");
+                process::exit(1);
+            }
             x += dx * dt;
             y += dy * dt;
             z += dz * dt;
-            points.push((x, z));
-            step += 1;
-        } else {
-            break;
-        }
-
-        terminal.draw(|f| {
-            let size = f.area();
-            let chart_area = Rect {
-                x: 0,
-                y: 0,
-                width: size.width,
-                height: size.height,
+            let point = match axis_choice.as_str() {
+                "xy" => (x, y),
+                "xz" => (x, z),
+                "yz" => (y, z),
+                _ => (x, y),
             };
 
-            let dataset = Dataset::default()
-                .marker(symbols::Marker::Dot)
-                .style(Style::default().fg(Color::Cyan))
-                .data(&points);
+            x_min = x_min.min(point.0);
+            x_max = x_max.max(point.0);
+            y_min = y_min.min(point.1);
+            y_max = y_max.max(point.1);
 
-            let chart = Chart::new(vec![dataset])
-                // .block(Block::default().borders(Borders::ALL).title("Lorenz Attractor (X-Z)"))
-                .x_axis(
-                    Axis::default() /*.title("X")*/
-                        .bounds([-20.0, 20.0]),
-                )
-                .y_axis(
-                    Axis::default() /*.title("Z")*/
-                        .bounds([0.0, 50.0]),
-                );
+            points.push(point);
+            // points.push((x, y));
+            step += 1;
 
-            f.render_widget(chart, chart_area);
-        })?;
+            terminal.draw(|f| {
+                let size = f.area();
+                let chart_area = Rect {
+                    x: 0,
+                    y: 0,
+                    width: size.width,
+                    height: size.height,
+                };
 
-        // adjust delay for animation
-        thread::sleep(Duration::from_millis(1));
+                let dataset = Dataset::default()
+                    .marker(symbols::Marker::Braille)
+                    .style(Style::default().fg(Color::Cyan))
+                    .data(&points);
 
-        if event::poll(Duration::from_millis(10))? {
-            if let event::Event::Key(key) = event::read()? {
-                if key.code == KeyCode::Esc || key.code == KeyCode::Char('q') {
-                    break;
+                let chart = Chart::new(vec![dataset])
+                    // .block(Block::default().borders(Borders::ALL).title("Lorenz Attractor (X-Z)"))
+                    .x_axis(
+                        Axis::default() /*.title("X")*/
+                            .bounds([x_min, x_max]),
+                    )
+                    .y_axis(
+                        Axis::default() /*.title("Z")*/
+                            .bounds([y_min, y_max]),
+                    );
+
+                f.render_widget(chart, chart_area);
+            })?;
+
+            // adjust delay for animation
+            // thread::sleep(Duration::from_millis(1));
+            if event::poll(Duration::from_millis(10))? {
+                if let event::Event::Key(key) = event::read()? {
+                    if key.code == KeyCode::Esc || key.code == KeyCode::Char('q') {
+                        break 'lorenz_attractor;
+                    }
                 }
             }
         }
     }
-
     disable_raw_mode()?;
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     Ok(())
